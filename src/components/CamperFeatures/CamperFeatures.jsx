2ce383422ccd1cf09ba4e05d03db3c042a135602
Andrii Krupa <krupaandrii@gmail.com>
2024-04-25 15:00:53 -0600

    Updated camper features for modal

--- src/components/CamperFeatures/CamperFeatures.jsx
+++ src/components/CamperFeatures/CamperFeatures.jsx
@@ -3,8 +3,9 @@ import { getCamperById } from '../../redux/camperts/operations';
 import css from './CamperFeatures.module.css';
 import { useEffect } from 'react';
 import { selectCamperByID } from '../../redux/selectors';
+import sprite from '../../images/sprite.svg';
 
-const CamperFeatures = ({ id }) => {
+const CamperFeatures = ({ id, onClick }) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -13,7 +14,67 @@ const CamperFeatures = ({ id }) => {
 
   const camper = useSelector(selectCamperByID);
 
-  return <div className={css.wrapper}>{camper?.name}</div>;
+  return (
+    <div className={css.containerModal}>
+      <div className={css.titleContainer}>
+        <h2 className={css.camperTitle}>{camper?.name}</h2>
+        <button type="button" onClick={onClick} className={css.cross}>
+          <svg width={32} height={32}>
+            <use href={sprite + '#icon-cross'} />
+          </svg>
+        </button>
+      </div>
+      <div className={css.cardContainer}>
+        <div className={css.cardReviews}>
+          <svg width={32} height={32}>
+            <use href={`${sprite}#icon-rating`} />
+          </svg>
+          <span>{`${camper?.rating}(${camper?.reviews.length} Reviews)`}</span>
+        </div>
+
+        <div className={css.cardLocation}>
+          <svg width={16} height={16}>
+            <use href={`${sprite}#icon-pin`} />
+          </svg>
+          <span>{camper?.location.split(', ').reverse().join(', ')}</span>
+        </div>
+      </div>
+
+      <div>
+        <p className={css.cardDescription}>{camper?.description}</p>
+      </div>
+
+      {/* <div className={css.scrollingArea}>
+        <div className={css.imageContainer}>
+          {camperData?.gallery.map((imageSrc, index) => (
+            <img
+              key={index}
+              src={imageSrc}
+              alt={camperData?.name}
+              className={css.image}
+            ></img>
+          ))}
+        </div>
+        <p>{camperData?.description}</p>
+      </div>
+      <div className={css.buttonsContainer}>
+        <button className={css.button} onClick={toggleActive}>
+          Features
+        </button>
+        <button className={css.button} onClick={toggleActive}>
+          Reviews
+        </button>
+      </div>
+      <div className={css.wrapper}>
+        {extended && isFeaturesOpen ? (
+          <Features data={parseDataForFeature(camperData)} />
+        ) : (
+          <Reviews data={camperData?.reviews} />
+        )}
+        {extended && <ModalForm />}
+      </div> */}
+    </div>
+  );
 };
 
 export default CamperFeatures;
